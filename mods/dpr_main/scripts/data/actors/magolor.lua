1b6bbe69ffc02b0539533ca2f876f4fb2639f5c3
Emilydev <104941875+YogaYiga@users.noreply.github.com>
2024-11-15 15:15:00 -0400

    File optimization

--- mods/dpr_main/scripts/data/actors/magolor.lua
+++ mods/dpr_main/scripts/data/actors/magolor.lua
@@ -2,47 +2,20 @@ local actor, super = Class(Actor, "magolor")
 
 function actor:init()
     super.init(self)
-
-    -- Display name (optional)
     self.name = "Magolor"
-
-    -- Width and height for this actor, used to determine its center
     self.width = 59
     self.height = 83
-
-    -- Hitbox for this actor in the overworld (optional, uses width and height by default)
     self.hitbox = {0, 50, 59, 33}
-
-    -- Color for this actor used in outline areas (optional, defaults to red)
-    self.color = {1, 0, 0}
-
-    -- Whether this actor flips horizontally (optional, values are "right" or "left", indicating the flip direction)
-    self.flip = nil
-
-    -- Path to this actor's sprites (defaults to "")
     self.path = "world/npcs/magolor"
-    -- This actor's default sprite or animation, relative to the path (defaults to "")
     self.default = "speen_1"
-
-    -- Path to this actor's portrait for dialogue (optional)
     self.portrait_path = "face/magolor"
-    -- Offset position for this actor's portrait (optional)
     self.portrait_offset = {-30, -6}
-
-    -- Whether this actor as a follower will blush when close to the player
-    self.can_blush = false
-
-    -- Table of talk sprites and their talk speeds (default 0.25)
     self.talk_sprites = nil
-
-    -- Table of sprite animations
     self.animations = {
         ["speen"] = {"speen", 0.01, true},
         ["speen_svfx"] = {"speen", 0.05, true},
     }
     self.talk_sprites = {}
-
-    -- Table of sprite offsets (indexed by sprite name)
     self.offsets = {
         ["speen"] = {0, 0}
     }
